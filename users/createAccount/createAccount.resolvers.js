44314749b06f5fd60ee47911d10cbcc6ebc50d51
Seoho Jung <seoho91@gmail.com>
2021-10-23 16:27:31 +0900

    Updated file upload feature for Edit Profile

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -10,27 +10,20 @@ export default {
       try {
         const existingUser = await client.user.findFirst({
           where: {
-            OR: [
-              {
-                username,
-              },
-              {
-                email,
-              },
-            ],
+            OR: [{ username }, { email }],
           },
         });
         if (existingUser) {
-          throw new Error("This username/password is already taken.");
+          throw new Error("This username/e-mail exists already.");
         }
-        const uglyPassword = await bcrypt.hash(password, 10);
+        const encrypterPassword = await bcrypt.hash(password, 10);
         return client.user.create({
           data: {
             username,
             email,
             firstName,
             lastName,
-            password: uglyPassword,
+            password: encrypterPassword,
           },
         });
       } catch (e) {
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,42 +1,59 @@
+import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      if (!loggedInUser) {
-        throw new Error("You are not logged in.")
-      };
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
+    editProfile: protectedResolver(
+      async (
+        _,
+        {
           firstName,
           lastName,
           username,
           email,
-          ...(uglyPassword && { password: uglyPassword }),
+          password: newPassword,
+          bio,
+          avatar,
         },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile.",
-        };
+        { loggedInUser, protectResolver }
+      ) => {
+        const { filename, createReadStream } = await avatar;
+        const readStream = createReadStream();
+        const writeStream = createWriteStream(
+          process.cwd() + "/uploads/" + filename
+        );
+        readStream.pipe(writeStream);
+
+        let encryptedPassword = null;
+        if (newPassword) {
+          encryptedPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            bio,
+            ...(encryptedPassword && { password: encryptedPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
       }
-    },
+    ),
   },
 };
