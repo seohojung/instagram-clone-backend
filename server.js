aa3e836a3cc73f5af6ffe47aa23a6e3f4dfc65ed
Seoho Jung <seoho91@gmail.com>
2021-10-23 17:27:00 +0900

    Switched from apollo-server to apollo-server-express for file uploads

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+// This line should come in the very beginning of the project to use virtual environment
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
+
+// Apollo server
 const server = new ApolloServer({
   resolvers,
   typeDefs,
@@ -14,6 +19,13 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is running on http://localhost:${PORT}`));
+// Express
+const app = express();
+app.use("/static", express.static("uploads"));
+
+// Connect Apollo server and Express
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -19,12 +19,17 @@ export default {
         },
         { loggedInUser, protectResolver }
       ) => {
-        const { filename, createReadStream } = await avatar;
-        const readStream = createReadStream();
-        const writeStream = createWriteStream(
-          process.cwd() + "/uploads/" + filename
-        );
-        readStream.pipe(writeStream);
+        let avatarUrl = null;
+        if (avatar) {
+          const { filename, createReadStream } = await avatar;
+          const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+          const readStream = createReadStream();
+          const writeStream = createWriteStream(
+            process.cwd() + "/uploads/" + newFilename
+          );
+          readStream.pipe(writeStream);
+          avatarUrl = `http://localhost:4000/static/${newFilename}`;
+        }
 
         let encryptedPassword = null;
         if (newPassword) {
@@ -41,6 +46,7 @@ export default {
             email,
             bio,
             ...(encryptedPassword && { password: encryptedPassword }),
+            ...(avatarUrl && { avatar: avatarUrl }),
           },
         });
         if (updatedUser.id) {
