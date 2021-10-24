6138038ff3b171005bf713615cdcf050c110516f
Seoho Jung <seoho91@gmail.com>
2021-10-24 22:22:55 +0900

    Added "search users" feature with pagination; added computed fields to "see profile"

---
+++ users/users.resolvers.js
@@ -0,0 +1,43 @@
+import client from "../client";
+
+export default {
+  User: {
+    totalFollowing: ({ id }) =>
+      // No need to await because we are directly returning this query without further processing
+      client.user.count({
+        where: {
+          followers: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+    totalFollowers: ({ id }) =>
+      client.user.count({
+        where: {
+          following: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+    isMyProfile: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return id === loggedInUser.id;
+    },
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+
+      const exists = await client.user
+        .findUnique({ where: { username: loggedInUser.username } })
+        .following({ where: { id } });
+      return exists.length !== 0;
+    },
+  },
+};
