bb820de13a8dfc8c9ea23748df0adf87f7977a21
Seoho Jung <seoho91@gmail.com>
2021-11-30 22:01:52 +0100

    Fixed login.typeDefs.js

--- users/login/login.typeDefs.js
+++ users/login/login.typeDefs.js
@@ -1,7 +1,12 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+  }
   type Mutation {
-    login(username: String!, password: String!): MutationResult!
+    login(username: String!, password: String!): LoginResult!
   }
 `;
